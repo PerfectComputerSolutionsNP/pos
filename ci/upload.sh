033a006c47db410314e193c0b656b48e7a089bed
Jabari Dash <jabari.dash@gmail.com>
2018-10-17 14:40:17 -0400

    upload.sh did not get pushed

---
+++ ci/upload.sh
@@ -0,0 +1,11 @@
+set -e
+
+if [ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]; then
+
+  docker build -t perfectcomputersolutionsnp/pos-api src/server
+  docker build -t perfectcomputersolutionsnp/pos-gui src/client
+  docker push perfectcomputersolutionsnp/pos-api
+  docker push perfectcomputersolutionsnp/pos-gui
+else
+  echo "Not pushing image to docker because $TRAVIS_BRANCH is not master branch"
+fi
