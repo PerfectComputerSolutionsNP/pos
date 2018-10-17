525d4eb1bfa6f1b51900de319c7a505f3e4cbb84
Jabari Dash <jabari.dash@gmail.com>
2018-10-17 14:47:21 -0400

    typo in script

--- ci/upload.sh
+++ ci/upload.sh
@@ -1,11 +1,16 @@
 set -e
 
-if [ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]; then
+if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+
+  echo "Building docker images"
 
   docker build -t perfectcomputersolutionsnp/pos-api src/server
   docker build -t perfectcomputersolutionsnp/pos-gui src/client
+
+  echo "Pushing docker images"
+
   docker push perfectcomputersolutionsnp/pos-api
   docker push perfectcomputersolutionsnp/pos-gui
 else
-  echo "Not pushing image to docker because $TRAVIS_BRANCH is not master branch"
+  echo "Not pushing image to docker because branch '$TRAVIS_BRANCH' is not master branch"
 fi
