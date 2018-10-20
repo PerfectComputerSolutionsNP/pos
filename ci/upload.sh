5099b4f138483eccc45d9f3b0f596ece738117ab
Jabari Dash <jabari.dash@gmail.com>
2018-10-20 09:52:46 -0400

    another attemp

--- ci/upload.sh
+++ ci/upload.sh
@@ -1,25 +1,43 @@
 set -e
 
-git config --global user.email "${GITHUB_EMAIL}"
-git config --global user.name "Perfect Computer Solutions"
+
+if [ $TRAVIS_BRANCH != "master" ] || [ $TRAVIS_PULL_REQUEST != "false" ];
+
+  echo "Not pushing image to docker because branch '$TRAVIS_BRANCH' is not master branch"
+  exit 0
+fi
 
 version="v0.0.0"
+text="Test release"
+branch="master"
+repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+token=${GITHUB_TOKEN}
 
-git tag -a -m "Releasing $version from Travis CI" $version
+generate_post_data()
+{
+  cat <<EOF
+{
+  "tag_name": "$version",
+  "target_commitish": "$branch",
+  "name": "$version",
+  "body": "$text",
+  "draft": false,
+  "prerelease": false
+}
+EOF
+}
 
-git push origin $version
+# GitHub release
+echo "Create release $version for repo: $repo_full_name branch: $branch"
+curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
 
-if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+# Docker release
+echo "Building docker images"
 
-  echo "Building docker images"
+docker build -t perfectcomputersolutionsnp/pos-api src/server:$version
+docker build -t perfectcomputersolutionsnp/pos-gui src/client:$version
 
-  docker build -t perfectcomputersolutionsnp/pos-api src/server:$version
-  docker build -t perfectcomputersolutionsnp/pos-gui src/client:$version
+echo "Pushing docker images"
 
-  echo "Pushing docker images"
-
-  docker push perfectcomputersolutionsnp/pos-api
-  docker push perfectcomputersolutionsnp/pos-gui
-else
-  echo "Not pushing image to docker because branch '$TRAVIS_BRANCH' is not master branch"
-fi
+docker push perfectcomputersolutionsnp/pos-api
+docker push perfectcomputersolutionsnp/pos-gui
