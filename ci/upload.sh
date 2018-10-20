ca2a5463cf1eb330c0c3b7e2334b41611be66e63
Jabari Dash <jabari.dash@gmail.com>
2018-10-20 10:18:41 -0400

    another attempt at builds

--- ci/upload.sh
+++ ci/upload.sh
@@ -7,8 +7,10 @@ if [ $TRAVIS_BRANCH != "master" ] || [ $TRAVIS_PULL_REQUEST == "true" ]; then
   # exit 0
 fi
 
+# TODO - Fetch latest version, bump it
+
 version="v0.0.0"
-text="Test release"
+text="Automated release from Travis CI"
 branch="master"
 repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
 token="${GITHUB_TOKEN}"
@@ -26,11 +28,9 @@ generate_post_data() {
 EOF
 }
 
-# GitHub release
 echo "Creating release $version for repo: $repo_full_name branch: $branch"
 curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
 
-# Docker release
 echo "Building docker images"
 docker build -t perfectcomputersolutionsnp/pos-api:$version src/server
 docker build -t perfectcomputersolutionsnp/pos-gui:$version src/client
